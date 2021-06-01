fe870d880a4c4b09762439ff5f6276f3d88dedb1
Laurence <laurencebalazo@gmail.com>
2021-06-01 21:15:32 +0800

    first commit

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {addTech} from '../../actions/techActions'
+
+
+const AddTechModal = ({addTech}) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+
+  const onSubmit = () => {
+    if(firstName === '' || lastName === '') {
+      M.toast({html: 'Please enter the first and last name'})
+    } else {
+      addTech({firstName, lastName})
+      M.toast({html: `${firstName} ${lastName} was added as tech.`})
+
+      setFirstName('')
+      setLastName('')
+    }
+  };
+
+  return (
+    <div id="add-tech-modal" className="modal">
+      <div className="modal-content">
+        <h4>New Technician</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+            <label htmlFor="firstName" className="active">
+              First Name
+            </label>
+          </div>
+
+          <div className="input-field">
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+            <label htmlFor="lastName" className="active">
+              First Name
+            </label>
+          </div>
+          
+
+          
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect waves-light blue btn"
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired,
+}
+
+
+export default connect(null, {addTech})(AddTechModal);
